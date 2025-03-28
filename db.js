ba431231cde078dd13b246aa85aeef27d864f2ab
Partha Maity <parthamaity2004@gmail.com>
2025-03-29 00:43:15 +0530

    next version of our code

--- db.js
+++ db.js
@@ -1,14 +1,19 @@
 const mongoose = require('mongoose');
 
 // define mongodb connection URL // Lecture no 7
-const mongoURL= 'mongodb://localhost:27017';
+// const mongoURL= 'mongodb://localhost:27017';// this is our local url
+const mongoURL = 'mongodb+srv://parthamaity2004:Partha988@cluster0.i5psrhz.mongodb.net/'
 
 // setup connection
 mongoose.connect(mongoURL, {
 
-    useNewUrlParser : true,
-    useUnifiedTopology : true
+    // useNewUrlParser : true,
+    // useUnifiedTopology : true,
 })
+mongoose.connect(mongoURL)
+  .then(() => console.log('✅ MongoDB connected successfully'))
+  .catch(err => console.error('❌ MongoDB connection error:', err));
+
 
 const db=mongoose.connection;
 
